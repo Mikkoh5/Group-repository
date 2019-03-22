911d204f94f65f687c0f74910fc37c84786a51c5
Mikko123 <mikko1718@student.hamk.fi>
2019-03-22 10:30:02 +0200

    laski  laku

--- src/gitCalc/Calculator.java
+++ src/gitCalc/Calculator.java
@@ -3,8 +3,10 @@ package gitCalc;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Anna luku 1: ");
+		
+		
 	}
 
 }
