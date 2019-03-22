ebd66b76fe6a1056296723d446ceb1352b4f12c1
Mikko123 <mikko1718@student.hamk.fi>
2019-03-22 10:34:22 +0200

    jee

--- src/gitCalc/src/src/Calculator.java
+++ src/gitCalc/src/src/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
 		int luku1;
 		int luku2;
 		
-		System.out.println("Anna 1. luku");
+		System.out.println("Anna 1. luku"); //lol
 		
 		Scanner sc = new Scanner(System.in);
 		
