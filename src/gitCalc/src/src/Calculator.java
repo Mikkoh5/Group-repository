4d00dcab77fadfcc3d9179875f144a5dcf728112
Mikko <mikko.hoikkala@student.hamk.fi>
2019-03-22 10:29:11 +0200

    Aloitettu laskin

---
+++ src/gitCalc/src/src/Calculator.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int luku1;
+		int luku2;
+		
+		System.out.println("Anna 1. luku");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc);
+		
+		//luku1 = System.in();
+		
+		// System.out.println(luku1);
+
+	}
+
+}
